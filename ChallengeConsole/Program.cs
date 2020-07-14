ef818d4f4c6c3d3db292ca6af3727f7b2cf192e9
inesCeh <ines.ceh@gmail.com>
2020-07-14 22:14:16 +0200

    Added additional metrics, Implemented method for storing data

--- ChallengeConsole/Program.cs
+++ ChallengeConsole/Program.cs
@@ -2,26 +2,25 @@
 using System.Timers;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Data.SQLite;
 using Hazelcast.Client;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
-using System.Diagnostics;
-
-
+using Hazelcast.Core;
 
 namespace ChallengeConsole
 {
     class Program
-    {       
+    {  
+        static IMap<string, HazelcastJsonValue> mapJsons;    
         private static System.Timers.Timer aTimer;
         static async Task Main(string[] args)
         {
+            CreateDatabase();
             await InitiateAuthorization();
         }
         private static void SetTimer()
         {
-            aTimer = new System.Timers.Timer(15000);
+            aTimer = new System.Timers.Timer(30000);
             aTimer.Elapsed += OnTimedEvent;
             aTimer.AutoReset = true;
             aTimer.Enabled = true;
@@ -31,8 +30,21 @@ namespace ChallengeConsole
         {
             Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
                           e.SignalTime);
-            await ReadData("googlefit", "body_weight", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+
+            //Googlefit
             await ReadData("googlefit", "body_height", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("googlefit", "body_weight", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("googlefit", "calories_burned", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("googlefit", "heart_rate", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("googlefit", "physical_activity", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("googlefit", "speed", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("googlefit", "step_count", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            //Fitbit
+            await ReadData("fitbit", "body_mass_index", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("fitbit", "body_weight", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+            await ReadData("fitbit", "physical_activity", "challengeines@gmail.com", "2020-07-12", "2020-07-13");
+
+            ReadFromDatabase();
         }
 
         private static async Task InitiateAuthorization() {
@@ -98,23 +110,7 @@ namespace ChallengeConsole
                             string data = await content.ReadAsStringAsync();
                             if (data != null)
                             {
-                                var dataObj = JObject.Parse(data);
-                                Console.WriteLine(data);  
-                                Console.WriteLine("Shim key : {0}", dataObj["shim"]);
-                                string body = $"{dataObj["body"]}";
-                                JArray jsonArray = JArray.Parse(body) as JArray;
-                                dynamic bodies = jsonArray;
-                                foreach (var item in bodies)
-                                {
-                                    if(string.Equals(endpoint, "body_weight")) {
-                                        Console.WriteLine("WEIGHT : {0}", item.body.body_weight.value.ToString());
-                                    } else if (string.Equals(endpoint, "body_height")) {
-                                        Console.WriteLine("HEIGHT : {0}", item.body.body_height.value.ToString());
-                                    } else {
-                                        Console.WriteLine("OTHER");
-                                    }
-                                }
-
+                                SaveJsonData(shimKey, endpoint, data);
                             }
                             else
                             {
@@ -125,6 +121,31 @@ namespace ChallengeConsole
                 }
             } catch(Exception exception) {
                 Console.WriteLine(exception);
+            }
+        }
+
+       private static void CreateDatabase() {
+           
+            var client = HazelcastClient.NewHazelcastClient();
+            mapJsons = client.GetMap<string, HazelcastJsonValue>("jsonData");
+       }
+
+        private static void SaveJsonData(string metricsSource, string metricsType, string jsonData) {
+
+            string key = metricsSource + metricsType;
+            mapJsons.Put(key, new HazelcastJsonValue(jsonData));
+            //client.Shutdown();
+        }
+
+        private static void ReadFromDatabase() 
+        {
+            ICollection<HazelcastJsonValue> jsons = mapJsons.Values();
+            foreach (var json in jsons)
+            {
+                Console.WriteLine();   
+                Console.WriteLine();   
+                Console.WriteLine("******************************************************************");
+                Console.WriteLine(json.ToString());
             }
         }
     }
